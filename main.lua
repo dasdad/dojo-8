376fe07345036d9646cb1f98e30c2cfe9a519ffd
Navarro <vitor.navarro87@gmail.com>
2013-07-18 20:59:14 -0300

    anagrama de aab

--- main.lua
+++ main.lua
@@ -1,11 +1,29 @@
 function anagramas(value)
 	--a =  value:sub(1,1)
-	if value:sub(1,1) == value:sub(2,2) then
-		return { value }
-	end
 	if #value == 2 then
+		if value:sub(1,1) == value:sub(2,2) then
+			return { value }
+		end
 		return {value, value:sub(2,2)..value:sub(1,1)}
 	end
+
+	if #value == 3 then
+		local fim = value:sub(3,3)
+		local resto = value:sub(1,2)
+		local navarro = {}
+		local resultado = {}
+
+		for i=1,#value do
+			navarro[resto:sub(1,i-1)..fim..resto:sub(i,#resto)] = ""
+		end
+		
+		for chave,valor in pairs(navarro) do
+			resultado[#resultado+1] = chave
+		end
+
+		return resultado
+	end
+
 	return { value }
 end
 
