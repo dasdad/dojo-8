73ce62a5ffaf5e884a1621c9af16583bcc5cfc98
Navarro <vitor.navarro87@gmail.com>
2013-07-18 20:37:03 -0300

    anagramas ab e ba e ca e duas letras iguais

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 function anagramas(value)
 	--a =  value:sub(1,1)
+	if value:sub(1,1) == value:sub(2,2) then
+		return { value }
+	end
 	if #value == 2 then
 		return {value, value:sub(2,2)..value:sub(1,1)}
 	end
