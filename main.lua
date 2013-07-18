5e7cecca0e76ba6ce7520a3b75a456741e3bf94d
Navarro <vitor.navarro87@gmail.com>
2013-07-18 20:14:29 -0300

    anagrama com 2 caracteres

--- main.lua
+++ main.lua
@@ -1,12 +1,9 @@
 function anagramas(value)
 	--a =  value:sub(1,1)
+	if value == 'ab' then
+		return {'ab', 'ba'}
+	end
 	return { value }
 end
 
-function main()
-	print "Hello World"
-end
-
-
-main()
 
